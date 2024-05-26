0c314135f75b67a7e76202060c033ee03b302c43
ClauChum <clauchum1433@gmail.com>
2024-05-26 01:13:44 -0600

    Habilidades

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-menu.onClick = () => {
+menu.onclick = () => {
     menu.classList.toggle('bx-x');
-    navbar.classList.toggle('open')
+    navbar.classList.toggle('open');
 };
 
 const sr = ScrollReveal({
